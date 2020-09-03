4440e34e5b3aa2e031a439ed0c8d581a9b13a9f4
babypoeder <sidhartamauri@hotmail.com>
2020-09-03 13:55:40 +0200

    laatste versie van herhaling hw

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+//herhaling
+$getal = 6;
+echo gettype($getal);
+echo "",PHP_EOL;
+
+//Oefening 1
+$klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
+print_r($klas);
+echo "",PHP_EOL;
+
+
+//Oefening 2
+for ($i=0; $i <4 ; $i++) {
+  print $klas[$i];
+  echo "",PHP_EOL;
+}
+
+//Oefening 3
+foreach ($klas as $value) {
+  echo "",PHP_EOL;
+  print $value;
+  echo "",PHP_EOL;
+}
+
+//Combinatie Opdracht
+$bucket_list =[];
+$hoeveelheid_activiteiten = readline("Hoeveel activiteiten wil je toevoegen aan de Bucket List?");
+for ($i=0; $i < $hoeveelheid_activiteiten; $i++) {
+  array_push($bucket_list, $activity = readline("Welke activiteiten wil je toevoegen?"));
+}
+
+print_r($bucket_list);
+
+foreach ($bucket_list as $value) {
+  print $value;
+  echo "",PHP_EOL;
+}
+
+
+?>
